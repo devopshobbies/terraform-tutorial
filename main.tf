0be2663f99b7d0824b6c8b7901559f180cdbf3c0
BabakDoraniarab <babakdoraniarab@jajafinance.com>
2023-01-29 21:44:27 +0200

    v006 is finished

--- main.tf
+++ main.tf
@@ -1,9 +1,19 @@
 resource "aws_instance" "babak" {
-  ami = "ami-08cd358d745620807"
-
+  ami           = "ami-08cd358d745620807"
   instance_type = "t2.micro"
+  key_name      = "dvh"
 
   tags = {
+    Name       = "babak"
     Created_By = "terraform"
   }
 }
+# resource "aws_instance" "babak2" {
+#   ami = "ami-08cd358d745620807"
+
+#   instance_type = "t2.micro"
+
+#   tags = {
+#     Created_By = "terraform"
+#   }
+# }
