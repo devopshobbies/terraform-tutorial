2f77c3add5029ddcbaed65163a2fb10ec5edee4e
BabakDoraniarab <babakdoraniarab@jajafinance.com>
2023-01-29 19:26:09 +0200

    we have used variables for the credentials

--- main.tf
+++ main.tf
@@ -10,6 +10,4 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-   access_key = "AKIA3OMONEGXTT3SUBXA"
-   secret_key = "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"
-}
+  }
