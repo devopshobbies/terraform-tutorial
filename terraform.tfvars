6f37e76364eeb4addd44836a6d9dc014d478580f
BabakDoraniarab <babakdoraniarab@jajafinance.com>
2023-02-20 10:21:04 +0200

    v009 terraform backend is added

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,3 +1,8 @@
+backend-bucket ="dvh-terraform-tutorial"
+backend-key ="build/airflow/terraform.tfstate"
+region = "eu-west-2"
+
+
 ami_id        = "ami-08cd358d745620807"
 instance_type = "t2.micro"
 
--- variables.tf
+++ variables.tf
@@ -1,13 +1,9 @@
-# variable "ami_id" { default = "ami-08cd358d745620807" }
-# # variable "ami_id" {}
-# variable "instance_type" {
-#   default = "t2.micro"
-# }
-# variable "key_name" { default = "dvh" }
-# variable "machine_name" { default = "babak" }
+variable "region" {}
+variable "backend-bucket" {}
+variable "backend-key" {}
 
 variable "ami_id" {
-  # default = "ami-08cd358d745620807"
+  default = "ami-08cd358d745620807"
   type        = string
   description = "this is ami id for the machine"
 }
