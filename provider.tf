d37df2222669903a9fe62f02d11d96cd6318d9e0
signorrayan <signor.rayan@gmail.com>
2023-03-11 22:56:10 +0100

    Splitting provider and backend

--- provider.tf
+++ provider.tf
@@ -1,19 +1,3 @@
-terraform {
-
- backend "s3" {
-    bucket = "dvh-terraform-tutorial"
-    key    = "build/airflow/terraform.tfstate"
-    region = "eu-west-2"
-  }
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
 # Configure the AWS Provider
 provider "aws" {
   region = var.region
