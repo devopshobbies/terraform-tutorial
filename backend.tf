2cad81065c06c37cf65860f52fb7320bf12ce42a
BabakDoraniarab <babakdoraniarab@jajafinance.com>
2023-03-15 11:41:39 +0200

    git backend

---
+++ backend.tf
@@ -0,0 +1,15 @@
+terraform {
+
+  backend "s3" {
+    bucket = "dvh-terraform-tutorial"
+    key    = "build/airflow/terraform.tfstate"
+    region = "eu-west-2"
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
