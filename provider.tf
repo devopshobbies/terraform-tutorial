c7efb230a091e8c350860f1dba275f87ac6a9150
BabakDoraniarab <babakdoraniarab@jajafinance.com>
2023-03-15 11:50:35 +0200

    provider

--- provider.tf
+++ provider.tf
@@ -1,4 +1,17 @@
+terraform {
+ backend "s3" {
+    bucket = "dvh-terraform-tutorial"
+    key    = "build/airflow/terraform.tfstate"
+    region = "eu-west-2"
+  }
 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
 # Configure the AWS Provider
 provider "aws" {
   region = var.region
