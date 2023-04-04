7524b64959e4a50b36f7347a8013b0de62e9e92a
BabakDoraniarab <babakdoraniarab@jajafinance.com>
2023-04-04 14:33:25 +0300

    v012 is finished

---
+++ locals.tf
@@ -0,0 +1,5 @@
+locals {
+  service_name = "teset"
+  owner        = "DevOps Team"
+  created_by = "terraform22"
+}
--- main.tf
+++ main.tf
@@ -1,30 +1,15 @@
-module "ec2_babak" {
-  source = "./module/ec2"
 
-  instance_name = "v011"
-  key_name      = "dvh"
+
+resource "aws_instance" "babak" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  count         = 4
+  tags = {
+    Name       = "babak-${var.env_name[count.index]}"
+    Created_By = local.created_by
+  }
 }
-
-
-module "ec2_babak2" {
-  source = "./module/ec2"
-
-  instance_name = "v011_2"
-  key_name      = "dvh"
-}
-
-
-
-# resource "aws_instance" "babak" {
-#   ami           = var.ami_id
-#   instance_type = var.instance_type
-#   key_name      = var.key_name
-
-#   tags = {
-#     Name       = var.machine_name
-#     Created_By = "terraform"
-#   }
-# }
 # resource "aws_instance" "babak2" {
 #   ami = "ami-08cd358d745620807"
 
@@ -35,6 +20,26 @@ module "ec2_babak2" {
 #     Name       = "Airflow"
 #   }
 # }
+
+
+# module "ec2_babak" {
+#   source = "./module/ec2"
+
+#   instance_name = "v011"
+#   key_name      = "dvh"
+# }
+
+
+# module "ec2_babak2" {
+#   source = "./module/ec2"
+
+#   instance_name = "v011_2"
+#   key_name      = "dvh"
+# }
+
+
+
+
 # terraform import aws_instance.babak3 i-01836e088c5f7f783
 # resource "aws_instance" "babak3" {
 #   ami = "ami-086b3de06dafe36c5"
